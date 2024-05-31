8a0a04cc4bed9a073e695a321c688f3d40055416
SakibSibly <sibly@duck.com>
2024-05-31 12:05:13 +0600

    login.html is added

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+class MainView(View):
+    def home(request):
+        return render(request, 'home/index.html')
--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+class MainView(View):
+    def login(request):
+        return render(request, 'login/index.html')
