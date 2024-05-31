bec0ce97b8c7b6bce9fce6a279f326b9f699a56d
SakibSibly <sibly@duck.com>
2024-05-31 11:44:26 +0600

    apps are registered

--- FakeNewsDetection/settings.py
+++ FakeNewsDetection/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
+    'create_account',
+    'home',
 ]
 
 MIDDLEWARE = [
