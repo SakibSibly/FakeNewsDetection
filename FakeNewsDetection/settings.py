5b4a5660a457054c85c614f4ce9eb66bd263fa6e
SakibSibly <sibly@duck.com>
2024-05-31 11:51:42 +0600

    folder structure is defined for project

--- FakeNewsDetection/settings.py
+++ FakeNewsDetection/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
     'create_account',
     'home',
+    'login',
 ]
 
 MIDDLEWARE = [
