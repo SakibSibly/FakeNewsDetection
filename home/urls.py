52d5e70dce1027c21b021325af577bfa35a15007
SakibSibly <sibly@duck.com>
2024-06-01 18:11:56 +0600

    url redirection is fixed

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MainView.showHomePage, name='main'),
+    path('', views.MainView.showHomePage, name='home'),
     # path('about/', views.MainView.showAboutPage, name='about'),
 ]
