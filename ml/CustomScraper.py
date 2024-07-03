4bb6a9aad35f170517900986aaba5c00fa8fe1b6
SakibSibly <sibly@duck.com>
2024-07-03 18:22:52 +0600

    env path resolved

--- ml/CustomScraper.py
+++ ml/CustomScraper.py
@@ -1,9 +1,11 @@
 import requests
 import os
+from dotenv import load_dotenv
 
 
 class CustomScraeper:
     def __init__(self, input_text: str) -> None:
+        load_dotenv()
         self.API_KEY=os.getenv('API_KEY')
         self.SEARCH_ENGINE_ID=os.getenv('SEARCH_ENGINE_ID')
         self.FILE_PATH='ml/result.txt'
