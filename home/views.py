0f70d3d325f61a916269e458a1c1c612918f4d14
SakibSibly <sibly@duck.com>
2024-07-12 08:26:23 +0600

    form feature modified for differentiability

--- home/views.py
+++ home/views.py
@@ -1,19 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
 from ml import CustomScraper as CS, SimilarityFinder as SF
 # Create your views here.
 
 class MainView(View):
     def showHomePage(request):
-        # THE REQUEST MAY CONFLICT WITH PRINT FEATURE
         if request.method == 'POST':
-            query = request.POST.get('query')
-            customScraper = CS.CustomScraeper(query)
-            customScraper.getNews()
-            similarityFinder = SF.SimilarityFinder(query)
-            similarityFinder.findSimilarity()
-            output = open('ml/final_output.txt', 'r', encoding='utf-8').read()
-            return render(request,'home/show.html',context={'content':output})
+            form_type = request.POST.get('form_type')
+            if form_type == 'check_news':
+                query = request.POST.get('query')
+                customScraper = CS.CustomScraeper(query)
+                customScraper.getNews()
+                similarityFinder = SF.SimilarityFinder(query)
+                similarityFinder.findSimilarity()
+                output = open('ml/final_output.txt', 'r', encoding='utf-8').read()
+                return render(request,'home/show.html',context={'content':output})
+            elif form_type == 'print_report':
+                # implement print feature
+                pass
             
         return render(request, 'home/index.html')
     
