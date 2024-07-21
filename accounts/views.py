e33556d813002899aa09b169a4326c3c3f8242ed
SakibSibly <sibly@duck.com>
2024-07-21 11:47:22 +0600

    view is splitted into many parts and unsuccessful login is handled

--- accounts/views.py
+++ accounts/views.py
@@ -4,36 +4,40 @@ from accounts.models import User
 import hashlib
 # Create your views here.
 
-class MainView(View):
-    def showLoginPage(request):
-        if request.method == 'POST':
-            data = request.POST
-            email = data.get('email')
-            password = hashlib.sha256(data.get('password').encode()).hexdigest()
+class LoginView(View):
 
-            database = User.objects.all()
-            for user in database:
-                if user.email == email and user.password == password:
-                    return redirect('home')
-
+    def get(self, request):
         return render(request, 'accounts/login.html')
     
-    def showRegisterPage(request):
-        if request.method == 'POST':
-            data = request.POST
-            name = data.get('name')
-            uname = data.get('uname')
-            email = data.get('email')
-            password = hashlib.sha256(data.get('password').encode()).hexdigest()
+    def post(self, request):
+        data = request.POST
+        email = data.get('email')
+        password = hashlib.sha256(data.get('password').encode()).hexdigest()
 
-            newUser = User(
-                name = name,
-                username = uname,
-                email = email,
-                password = password,
-            )
-            newUser.save()
-            return render(request, 'accounts/success.html')
+        database = User.objects.all()
+        for user in database:
+            if user.email == email and user.password == password:
+                return redirect('home')
+        
+        return render(request, 'accounts/unsuccessful_login.html')
 
+class RegisterView(View):
+
+    def get(self, request):
         return render(request, 'accounts/register.html')
     
+    def post(self, request):
+        data = request.POST
+        name = data.get('name')
+        uname = data.get('uname')
+        email = data.get('email')
+        password = hashlib.sha256(data.get('password').encode()).hexdigest()
+
+        newUser = User(
+            name = name,
+            username = uname,
+            email = email,
+            password = password,
+        )
+        newUser.save()
+        return render(request, 'accounts/success.html')
