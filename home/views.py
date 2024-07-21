597b966d0283683c0038f5a8ffe81b8be2b59eb4
SakibSibly <sibly@duck.com>
2024-07-21 12:19:37 +0600

    Indentation fixed

--- home/views.py
+++ home/views.py
@@ -9,21 +9,20 @@ class HomeView(View):
         return render(request, 'home/index.html')
     
     def post(self, request):
-        if request.method == 'POST':
-            form_type = request.POST.get('form_type')
-            if form_type == 'check_news':
-                query = request.POST.get('query')
-                customScraper = CS.CustomScraeper(query)
-                customScraper.getNews()
-                similarityFinder = SF.SimilarityFinder(query)
-                similarityFinder.findSimilarity()
-                output = open('ml/final_output.txt', 'r', encoding='utf-8').read()
-                return render(request,'home/show.html',context={'content':output})
-            elif form_type == 'print_report':
-                # implement print feature
-                return HttpResponse("Print feature is not implemented yet!")
+        form_type = request.POST.get('form_type')
+        if form_type == 'check_news':
+            query = request.POST.get('query')
+            customScraper = CS.CustomScraeper(query)
+            customScraper.getNews()
+            similarityFinder = SF.SimilarityFinder(query)
+            similarityFinder.findSimilarity()
+            output = open('ml/final_output.txt', 'r', encoding='utf-8').read()
+            return render(request,'home/show.html',context={'content':output})
+        elif form_type == 'print_report':
+            # implement print feature
+            return HttpResponse("Print feature is not implemented yet!")
 
 class AboutView(View):
         
-        def get(self, request): 
-            return HttpResponse("Implement the About us page with a new tab window")
+    def get(self, request): 
+        return HttpResponse("Implement the About us page with a new tab window")
