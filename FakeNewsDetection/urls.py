f96dde61de7057c518e5b36997c977a0e13023a5
SakibSibly <sibly@duck.com>
2024-09-27 06:40:16 +0600

    Debugging feature for localhost is implemented

--- FakeNewsDetection/urls.py
+++ FakeNewsDetection/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('account/', include('accounts.urls')),
-    path('home/', include('home.urls')),
-]
+    path('accounts/', include('accounts.urls')),
+] + debug_toolbar_urls()
