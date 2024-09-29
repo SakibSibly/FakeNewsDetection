ce0ace4463b5f423e7beaaec88425b1c4cbcdcb4
SakibSibly <sibly@duck.com>
2024-09-29 19:41:07 +0600

    Documentation url added with respective view

--- documentations/views.py
+++ documentations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class DocumentationView(View):
+    def get(self, request):
+        return render(request, 'documentations/documentation.html')
+    
+    def post(self, request):
+        pass
