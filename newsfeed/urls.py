682e9eeb592256bc37ca0b2dfb09804afc913bb0
SakibSibly <sibly@duck.com>
2024-09-29 19:36:25 +0600

    newsfeed, newsfeed_detail url refactored to news_feed, news_feed_detail

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('newsfeed/', views.NewsFeedView.as_view(), name='newsfeed'),
-    path('newsfeed/<int:pk>/', views.NewsFeedDetailView.as_view(), name='newsfeed_detail'),
+    path('newsfeed/', views.NewsFeedView.as_view(), name='news_feed'),
+    path('newsfeed/<int:pk>/', views.NewsFeedDetailView.as_view(), name='news_feed_detail'),
 ]
