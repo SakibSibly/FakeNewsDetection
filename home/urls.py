d07ab78d1cce43868bfc1103fd20b4a092f3c45a
SakibSibly <sibly@duck.com>
2024-10-01 19:52:46 +0600

    URL home and nullhome added

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='nullhome'),
+    path('home/', views.HomeView.as_view(), name='home'),
 ]
