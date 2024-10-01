94156b9bdb9123a7439d976b301175ac39d912b3
SakibSibly <sibly@duck.com>
2024-10-01 19:51:35 +0600

    Individual news article view to the user is maintained

--- newsfeed/urls.py
+++ newsfeed/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.NewsFeedView.as_view(), name='news_feed'),
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news_detail'),
 ]
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from .models import News
 
 
 class NewsFeedView(View):
     def get(self, request):
         return render(request, 'newsfeed/newsfeed.html')
-    
-    def post(self, request):
-        pass
+
+
+class NewsDetailView(View):
+    def get(self, request, pk):
+        news = News.objects.filter(pk=pk).first()
+        return render(request, 'newsfeed/individual_newsfeed.html', {'news': news})
