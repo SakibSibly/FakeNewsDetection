52bb4d5bba827e8b9495b5b7d368ea6754105993
SakibSibly <sibly@duck.com>
2024-10-04 00:11:51 +0600

    Password reset feature added to the Project

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,24 @@ urlpatterns = [
     path('profile/<str:username>/', views.UserProfileView.as_view(), name='profile'),
     path('print/', views.PrintView.as_view(), name='print_report'),
     path('history/<str:username>/', views.UserHistoryView.as_view(), name='history'),
+    
+    # Password reset request (Forgot Password)
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html'), 
+        name='password_reset'),
+
+    # Password reset email sent
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'), 
+        name='password_reset_done'),
+
+    # Password reset confirmation link
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'), 
+        name='password_reset_confirm'),
+
+    # Password successfully reset
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'), 
+        name='password_reset_complete'),
 ]
